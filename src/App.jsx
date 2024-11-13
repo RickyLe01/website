14c455c45ef98ed16e15dc1f3f703ea9fd18f357
RickyLe01 <RickyDangLe01@gmail.com>
2024-11-12 21:29:49 -0800

    new routers

--- src/App.jsx
+++ src/App.jsx
@@ -6,20 +6,20 @@ import Services from './Components/Services/Services'
 import MyWork from './Components/MyWork/MyWork'
 import Contact from './Components/Contact/Contact'
 import Footer from './Components/Footer/Footer'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Router, Routes, Route } from 'react-router-dom'
 import Resume from "./pages/Resume/Resume.jsx"
 import Projects from "./pages/Projects/Projects.jsx"
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/resume" element={<Resume />} />
         <Route path="/projects" element={<Projects />} />
       </Routes>
       <Footer />
-    </BrowserRouter>
+    </Router>
   );
 }
 
